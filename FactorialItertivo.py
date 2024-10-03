847222a4e137ad47811a434fd1a5cd0876b91823
Cristian1667 <145683765+Cristian1667@users.noreply.github.com>
2024-10-03 14:30:41 -0600

    Create FactorialItertivo.py

---
+++ FactorialItertivo.py
@@ -0,0 +1,9 @@
+def factorial_iterativo(n):
+    resultado = 1
+    for i in range(1, n + 1):
+        resultado *= i
+    return resultado
+
+# Ejemplo de uso
+numero = 5
+print(f"El factorial de {numero} es: {factorial_iterativo(numero)}")
